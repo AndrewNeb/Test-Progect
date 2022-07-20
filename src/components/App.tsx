fcd01e7cbd8b4ef0c7a31386116c703bdb9c3d75
Андрей Владимирович Небышинец <buh-2@mtrans.local>
2022-07-20 10:51:06 +0300

    -Updated with ButtonProps component

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Button from "./Button/Button";
+import NewButton from "./ButtonProps/ButtonProps";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>My React App</h1>
+      <Button/>
+      <NewButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonProps/ButtonProps.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ButtonProps.css';
+
+const NewButton = () => {
+    return <button className="my-newButton">New Button</button>;
+};
+
+export default NewButton;
